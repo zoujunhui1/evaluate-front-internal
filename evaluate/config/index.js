46af2c5de804de3454ecd945b0fdd402b9ddd619
zjh <zoujunhui@bilibili.co>
2021-11-25 14:54:52 +0800

    host

--- evaluate/config/index.js
+++ evaluate/config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    // host: 'www.qinbi.shopping', // can be overwritten by process.env.HOST
+    // host: 'localhost', // can be overwritten by process.env.HOST
+    host: '150.109.150.224', // can be overwritten by process.env.HOST
     port: 8090, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
